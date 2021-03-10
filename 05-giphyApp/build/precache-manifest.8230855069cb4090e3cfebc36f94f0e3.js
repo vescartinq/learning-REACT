65eaa35702b793f795b18066cb68485dc325f85e
Starkin28 <victoreq00@gmail.com>
2021-03-10 19:32:13 +0100

    added build folder

---
+++ 05-giphyApp/build/precache-manifest.8230855069cb4090e3cfebc36f94f0e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15e4c7378a5792aa4b719662ca0d46b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2668082d250ef28cc661",
+    "url": "/static/css/main.4df16565.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2668082d250ef28cc661",
+    "url": "/static/js/main.2596e530.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
