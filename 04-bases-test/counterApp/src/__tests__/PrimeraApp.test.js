e414330b8a570fd86fd0be8a3379be1a1c58f632
Starkin28 <victoreq00@gmail.com>
2021-03-10 16:53:03 +0100

    PrimeraApp and CounterApp tested

--- 04-bases-test/counterApp/src/__tests__/PrimeraApp.test.js
+++ 04-bases-test/counterApp/src/__tests__/PrimeraApp.test.js
@@ -1,14 +1,38 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+// import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { shallow } from 'enzyme';
 
 import PrimeraApp from '../PrimeraApp';
 
-describe('PrimeraApp', () => {
-  test('should show message "Hola, soy Goku', () => {
+describe.skip('PrimeraApp', () => {
+  // ------------JEST-----------
+  // test('should show message "Hola, soy Goku', () => {
+  //   const greeting = 'Hola, soy Goku';
+
+  //   const wrapper = render(<PrimeraApp saludo={greeting} />);
+
+  //   expect(wrapper.getByText(greeting)).toBeInTheDocument();
+  // });
+
+  // -----------ENZYME----------
+  test('should show PrimeraApp component correctly', () => {
     const greeting = 'Hola, soy Goku';
+    const wrapper = shallow(<PrimeraApp saludo={greeting} />);
 
-    const wrapper = render(<PrimeraApp saludo={greeting} />);
+    expect(wrapper).toMatchSnapshot();
+  });
 
-    expect(wrapper.getByText(greeting)).toBeInTheDocument();
+  test('should show the subtitle sent by props', () => {
+    const greeting = 'Hola, soy Goku';
+    const subtitle = 'Soy el subtitulo';
+    const wrapper = shallow(
+      <PrimeraApp saludo={greeting} subtitulo={subtitle} />
+    );
+
+    const textoParrafo = wrapper.find('p').text();
+    // console.log(textoParrafo);
+
+    expect(textoParrafo).toBe(subtitle);
   });
 });
--- 04-bases-test/counterApp/src/index.js
+++ 04-bases-test/counterApp/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import PrimeraApp from './PrimeraApp'
+// import PrimeraApp from './PrimeraApp';
 import CounterApp from './CounterApp';
 
 import './index.css';
 
-
 const divRoot = document.querySelector('#app');
 
-ReactDOM.render( <CounterApp value={ 10 } />  , divRoot );
-
+// ReactDOM.render(<PrimeraApp saludo="Hola, soy Goku" />, divRoot);
+ReactDOM.render(<CounterApp value={10} />, divRoot);
