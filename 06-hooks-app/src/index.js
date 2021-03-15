726e1b1a0dafca08185d7716a549a236e93e4cb2
Starkin28 <victoreq00@gmail.com>
2021-03-15 20:49:52 +0100

    todoApp

--- 06-hooks-app/src/index.js
+++ 06-hooks-app/src/index.js
@@ -13,12 +13,7 @@ import ReactDOM from 'react-dom';
 // import { MemoHook } from './components/06-memos/MemoHook';
 // import { CallbackHook } from './components/06-memos/CallbackHook';
 // import { Padre } from './components/07-tarea-memo/Padre';
-// import { TodoApp } from './components/08-useReducer/TodoApp';
-import { MainApp } from './components/09-useContext/MainApp';
+import { TodoApp } from './components/08-useReducer/TodoApp';
+// import { MainApp } from './components/09-useContext/MainApp';
 
-
-
-ReactDOM.render(
-    <MainApp />,
-  document.getElementById('root')
-);
+ReactDOM.render(<TodoApp />, document.getElementById('root'));
