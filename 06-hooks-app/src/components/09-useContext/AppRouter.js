ba9736756240e1f6146edb117e7781a4be422d73
Starkin28 <victoreq00@gmail.com>
2021-03-16 09:47:34 +0100

    refactoring code changes

--- 06-hooks-app/src/components/09-useContext/AppRouter.js
+++ 06-hooks-app/src/components/09-useContext/AppRouter.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Redirect,
-  } from 'react-router-dom';
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 
 import { NavBar } from './NavBar';
 
@@ -13,24 +13,22 @@ import { LoginScreen } from './LoginScreen';
 import { HomeScreen } from './HomeScreen';
 
 export const AppRouter = () => {
-    return (
-        <Router>
-            <div>
-                
-                <NavBar />
+  return (
+    <Router>
+      <div>
+        <NavBar />
 
-                <div className="container">
-                    <Switch>
-                        <Route exact path="/" component={ HomeScreen } />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={HomeScreen} />
 
-                        <Route exact path="/about" component={ AboutScreen } />
-                        <Route exact path="/login" component={ LoginScreen } />
-                        
-                        <Redirect to="/" />
+            <Route exact path="/about" component={AboutScreen} />
+            <Route exact path="/login" component={LoginScreen} />
 
-                    </Switch>
-                </div>
-            </div>
-        </Router>
-    )
-}
+            <Redirect to="/" />
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
+};
