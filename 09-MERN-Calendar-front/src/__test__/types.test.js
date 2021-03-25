2c166f0ef62f91440f861efe8150474db5200eb3
Starkin28 <victoreq00@gmail.com>
2021-03-25 17:00:53 +0100

    types.test.js

---
+++ 09-MERN-Calendar-front/src/__test__/types.test.js
@@ -0,0 +1,27 @@
+import { types } from '../types/types';
+
+describe('types', () => {
+  test('should have this types', () => {
+    expect(types).toEqual({
+      uiOpenModal: '[ui] Open modal',
+      uiCloseModal: '[ui] Close modal',
+
+      eventSetActive: '[event] Set Active',
+      eventLogout: '[event] Logout event',
+
+      evetStartAddNew: '[event] Start add new',
+      eventAddNew: '[event] Add new',
+      eventClearActiveEvent: '[event] Clear active event',
+      eventUpdated: '[event] Event updated',
+      eventDeleted: '[event] Event deleted',
+      eventLoaded: '[event] Events loaded',
+
+      authCheckingFinish: '[auth] Finish checking login state',
+      authStartLogin: '[auth] Start login',
+      authLogin: '[auth] Login',
+      authStartRegister: '[auth] Start Register',
+      authStartStartTokenRenew: '[auth] Start token renew',
+      authLogout: '[auth] Logout',
+    });
+  });
+});
