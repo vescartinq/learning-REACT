3c6e71f001fe51cd413032062aef2f7ecc6c6a27
Starkin28 <victoreq00@gmail.com>
2021-03-25 15:30:53 +0100

    deployment of front & back on heroku

---
+++ 10-MERN-Calendar-back/public/precache-manifest.4d78f276f35558d5221267b4fb53063f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af3db869e55627dbc7ccd1b917183f34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04638f3313c707e17829",
+    "url": "/static/css/2.59a0ab78.chunk.css"
+  },
+  {
+    "revision": "61c4bb5c2c461cc47ec1",
+    "url": "/static/css/main.5cf50011.chunk.css"
+  },
+  {
+    "revision": "04638f3313c707e17829",
+    "url": "/static/js/2.7c8777c6.chunk.js"
+  },
+  {
+    "revision": "fa4b4056bba234820139274405351863",
+    "url": "/static/js/2.7c8777c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61c4bb5c2c461cc47ec1",
+    "url": "/static/js/main.0d562904.chunk.js"
+  },
+  {
+    "revision": "c2ab9c45931d577cf16e",
+    "url": "/static/js/runtime-main.27b01e2e.js"
+  }
+]);
