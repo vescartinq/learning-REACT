01929190bc1f0452174a86875da83f136737d403
Starkin28 <victoreq00@gmail.com>
2021-03-25 12:58:31 +0100

    login merged

--- 09-MERN-Calendar-front/src/components/auth/LoginScreen.js
+++ 09-MERN-Calendar-front/src/components/auth/LoginScreen.js
@@ -1,79 +1,131 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useForm } from '../../hooks/useForm';
+import { startLogin, startRegister } from '../../actions/auth';
+import Swal from 'sweetalert2';
+
 import './login.css';
 
 export const LoginScreen = () => {
-    return (
-        <div className="container login-container">
-            <div className="row">
-                <div className="col-md-6 login-form-1">
-                    <h3>Ingreso</h3>
-                    <form>
-                        <div className="form-group">
-                            <input 
-                                type="text"
-                                className="form-control"
-                                placeholder="Correo"
-                            />
-                        </div>
-                        <div className="form-group">
-                            <input
-                                type="password"
-                                className="form-control"
-                                placeholder="Contraseña"
-                            />
-                        </div>
-                        <div className="form-group">
-                            <input 
-                                type="submit"
-                                className="btnSubmit"
-                                value="Login" 
-                            />
-                        </div>
-                    </form>
-                </div>
+  const dispatch = useDispatch();
 
-                <div className="col-md-6 login-form-2">
-                    <h3>Registro</h3>
-                    <form>
-                        <div className="form-group">
-                            <input
-                                type="text"
-                                className="form-control"
-                                placeholder="Nombre"
-                            />
-                        </div>
-                        <div className="form-group">
-                            <input
-                                type="email"
-                                className="form-control"
-                                placeholder="Correo"
-                            />
-                        </div>
-                        <div className="form-group">
-                            <input
-                                type="password"
-                                className="form-control"
-                                placeholder="Contraseña" 
-                            />
-                        </div>
+  const [formLoginValues, handleLoginInputChange] = useForm({
+    lEmail: '',
+    lPassword: '',
+  });
 
-                        <div className="form-group">
-                            <input
-                                type="password"
-                                className="form-control"
-                                placeholder="Repita la contraseña" 
-                            />
-                        </div>
+  const [formRegisterValues, handleRegisterInputChange] = useForm({
+    rName: 'Noa',
+    rEmail: 'noa@gmail.com',
+    rPassword1: '123456',
+    rPassword2: '123456',
+  });
 
-                        <div className="form-group">
-                            <input 
-                                type="submit" 
-                                className="btnSubmit" 
-                                value="Crear cuenta" />
-                        </div>
-                    </form>
-                </div>
+  const { lEmail, lPassword } = formLoginValues;
+  const { rName, rEmail, rPassword1, rPassword2 } = formRegisterValues;
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    dispatch(startLogin(lEmail, lPassword));
+  };
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+
+    if (rPassword1 !== rPassword2) {
+      return Swal.fire(
+        'Error',
+        'Las contraseñas deben de ser iguales',
+        'error'
+      );
+    }
+    // console.log('?');
+    dispatch(startRegister(rEmail, rPassword1, rName));
+  };
+
+  return (
+    <div className="container login-container">
+      <div className="row">
+        <div className="col-md-6 login-form-1">
+          <h3>Ingreso</h3>
+          <form onSubmit={handleLogin}>
+            <div className="form-group">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Correo"
+                name="lEmail"
+                value={lEmail}
+                onChange={handleLoginInputChange}
+              />
             </div>
+            <div className="form-group">
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Contraseña"
+                name="lPassword"
+                value={lPassword}
+                onChange={handleLoginInputChange}
+              />
+            </div>
+            <div className="form-group">
+              <input type="submit" className="btnSubmit" value="Login" />
+            </div>
+          </form>
         </div>
-    )
-}
+
+        <div className="col-md-6 login-form-2">
+          <h3>Registro</h3>
+          <form onSubmit={handleRegister}>
+            <div className="form-group">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Nombre"
+                name="rName"
+                value={rName}
+                onChange={handleRegisterInputChange}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Correo"
+                name="rEmail"
+                value={rEmail}
+                onChange={handleRegisterInputChange}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Contraseña"
+                name="rPassword1"
+                value={rPassword1}
+                onChange={handleRegisterInputChange}
+              />
+            </div>
+
+            <div className="form-group">
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Repita la contraseña"
+                name="rPassword2"
+                value={rPassword2}
+                onChange={handleRegisterInputChange}
+              />
+            </div>
+
+            <div className="form-group">
+              <input type="submit" className="btnSubmit" value="Crear cuenta" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
--- 09-MERN-Calendar-front/src/router/AppRouter.js
+++ 09-MERN-Calendar-front/src/router/AppRouter.js
@@ -1,25 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Redirect
-  } from 'react-router-dom';
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+
+import { useDispatch, useSelector } from 'react-redux';
 
 import { LoginScreen } from '../components/auth/LoginScreen';
 import { CalendarScreen } from '../components/calendar/CalendarScreen';
+import { startChecking } from '../actions/auth';
 
 export const AppRouter = () => {
-    return (
-        <Router>
-            <div>
-                <Switch>
-                    <Route exact path="/login" component={ LoginScreen } />      
-                    <Route exact path="/" component={ CalendarScreen } />
+  const dispatch = useDispatch();
+  const { checking, uid } = useSelector((state) => state.auth);
 
-                    <Redirect to="/" />   
-                </Switch>
-            </div>
-        </Router>
-    )
-}
+  useEffect(() => {
+    dispatch(startChecking());
+  }, [dispatch]);
+
+  if (checking) {
+    return <h5>Espere...</h5>;
+  }
+
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/login" component={LoginScreen} />
+          <Route exact path="/" component={CalendarScreen} />
+
+          <Redirect to="/" />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
